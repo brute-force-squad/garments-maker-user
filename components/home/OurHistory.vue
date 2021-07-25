d224638b95636eb53013b74aae61055d538e432f
zubayerhimel <zhimel007@gmail.com>
2021-07-25 22:03:27 +0600

    done with home page

--- components/home/OurHistory.vue
+++ components/home/OurHistory.vue
@@ -1,30 +1,54 @@
 <template>
   <div>
     <v-row>
-      <v-col cols="12" md="6">
-        <custom-paragraph>
-          <p>
-            In 2000 our company has initiated and developed. The concept of”
-            Garments Maker Ltd”. Our first domain of intervention was garments
-            which are presently our main activity. Since our creation we have
-            been developing our expertise in the garments industry. 18 years of
-            experience has largely validated our concept and placed us as a
-            leader in this field. Through every day solving sourcing problems
-            for our long-term customers, we have developed in-depth knowledge in
-            the main fashion items we sell and continue to evolve according to
-            the end customer and purchasing needs.
-          </p>
-        </custom-paragraph>
+      <v-col cols="12">
+        <div class="display-2 mt-12 mb-6">Our Strategy</div>
+        <p class="bold">
+          Our vision is simple but bold – we aspire to create the supply chain
+          of the future to help our customers navigate the digital economy and
+          to make life better for one billion people in the supply chain.
+        </p>
+        <p>
+          In 2000 our company has initiated and developed. The concept of”
+          Garments Maker Ltd”. Our first domain of intervention was garments
+          which are presently our main activity. Since our creation we have been
+          developing our expertise in the garments industry. 18 years of
+          experience has largely validated our concept and placed us as a leader
+          in this field. Through every day solving sourcing problems for our
+          long-term customers, we have developed in-depth knowledge in the main
+          fashion items we sell and continue to evolve according to the end
+          customer and purchasing needs.
+        </p>
       </v-col>
-      <v-col cols="12" md="6">
-        <v-img
-          class="mx-auto my-auto"
-          max-height="400"
-          max-width="500"
-          lazy-src="https://firebasestorage.googleapis.com/v0/b/garments-maker.appspot.com/o/lazy-leaf-cloth.png?alt=media&token=b3f72371-e81f-411d-99ee-fbac4df484cc"
-          src="https://firebasestorage.googleapis.com/v0/b/garments-maker.appspot.com/o/leaf-cloth.png?alt=media&token=5d5ed11d-26b4-47f6-b1ca-13bc530683a8"
-          contain
-        />
+      <v-col cols="12" sm="6">
+        <v-card flat class="card">
+          <v-card-title>
+            <v-icon class="icon strategy-icon" color="secondary">
+              mdi-strategy</v-icon
+            >
+          </v-card-title>
+          <v-card-text>
+            Capturing and sharing data-driven insights across the entire value
+            chain will help us better predict and interact with our customers
+            faster and more creatively. Speed, innovation and digitalization are
+            key to building the supply chain of the future.
+            <div class="title mt-4">STRATEGIC DIRECTION</div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-card flat class="card">
+          <v-card-title>
+            <v-icon class="icon" color="secondary"> mdi-autorenew</v-icon>
+          </v-card-title>
+          <v-card-text>
+            We responsibly manage our environmental, social and governance
+            performance and work with our customers, suppliers and industry
+            partners to further the sustainability of supply chains and
+            communities across our ecosystem.
+            <div class="title mt-4">SUSTAINABILITY STRATEGY</div>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </div>
@@ -37,4 +61,25 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+  transition: all 0.5s;
+
+  .title {
+    opacity: 0;
+  }
+
+  &:hover {
+    .title {
+      opacity: 1;
+    }
+  }
+}
+.display-2,
+.title {
+  font-family: 'Poppins', sans-serif !important;
+}
+.icon {
+  font-size: 2.5em;
+}
+</style>
--- components/home/SupplyChainSolution.vue
+++ components/home/SupplyChainSolution.vue
@@ -1,5 +1,39 @@
 <template>
-  <div>
+  <div class="mt-12">
+    <v-row>
+      <v-col cols="12">
+        <div class="display-1 mb-4">Supply Chain Innovation</div>
+        <p class="bold">
+          We convene the global supply chain creating customized, end-to-end
+          supply chain and logistics solutions for brands and retailers. What
+          sets us apart is our high degree of flexibility and scalability which
+          is unrivaled in our industry.
+        </p>
+        <p>
+          Millions of consumer goods pass through Li & Fung’s supply chain every
+          year. For that to happen, we connect thousands of suppliers and
+          vendors with leading brands and retailers, all with the goal of
+          meeting consumer demand.
+        </p>
+        <div class="title my-6">
+          We are creating the supply chain of the future
+        </div>
+        <p>
+          Traditional supply chain management is undergoing massive change
+          driven by disruptive technologies in the retail industry.
+        </p>
+        <p>
+          Starting with product design and development and including everything
+          from compliance to raw material and factory sourcing, manufacturing
+          control, logistics, and more, we offer end-to-end services for all
+          stakeholders in the consumer goods industry. Our offices and teams are
+          based in all key production markets, giving us the expertise and
+          extensive network of supply chain partners needed to meet the diverse
+          needs of our global customers.
+        </p>
+      </v-col>
+    </v-row>
+
     <v-img
       lazy-src="https://firebasestorage.googleapis.com/v0/b/garments-maker.appspot.com/o/lazy-supply-chain-solution.svg?alt=media&token=b4154af1-0e83-4a5f-8e2a-7573c62b8689"
       src="https://firebasestorage.googleapis.com/v0/b/garments-maker.appspot.com/o/supply-chain-solution.svg?alt=media&token=efa3773a-d107-4e03-9994-e6bab775395a"
@@ -24,4 +58,10 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.display-1,
+.title {
+  font-family: 'Poppins', sans-serif !important;
+  font-weight: 600 !important;
+}
+</style>
